19d60459b1a78a77326448e1003fe1ca035372b1
bahaa <malkebahaa450>
2025-02-21 07:20:18 +0200

    commit_2 just a commit

---
+++ My_Flappy_Bird_C_Sharp_V2/My_Flappy_Bird_C_Sharp_V2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace My_Flappy_Bird_C_Sharp_V2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
